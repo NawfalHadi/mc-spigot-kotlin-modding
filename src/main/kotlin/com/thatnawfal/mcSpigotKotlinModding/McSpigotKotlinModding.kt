8e03c0b142c12e05fd4bcfc31a13282dcbf10919
NawfalHadi <nawfalhdi@gmail.com>
2025-06-25 18:17:21 +0800

    init : behaviour as a sample, new thing, back to here, create branch

---
+++ src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
@@ -0,0 +1,14 @@
+package com.thatnawfal.mcSpigotKotlinModding
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class McSpigotKotlinModding : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
