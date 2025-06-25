80f594c88033f3c17b709b2c86ba8a2d90acfd2b
NawfalHadi <nawfalhdi@gmail.com>
2025-06-25 21:21:55 +0800

    effects : fireworks and potion

--- src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
+++ src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
@@ -1,11 +1,44 @@
 package com.thatnawfal.mcSpigotKotlinModding
 
-import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.Bukkit
+import org.bukkit.Color
+import org.bukkit.FireworkEffect
 
-class McSpigotKotlinModding : JavaPlugin() {
+import org.bukkit.entity.Firework
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerToggleSneakEvent
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+
+class McSpigotKotlinModding : JavaPlugin(), Listener {
 
     override fun onEnable() {
-        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this)
+    }
+
+    @EventHandler
+    private fun onSneak(p: PlayerToggleSneakEvent){
+        val firework = p.player.world.spawn(p.player.location, Firework::class.java)
+        val meta = firework.fireworkMeta
+
+        meta.addEffect(FireworkEffect.builder()
+            .withColor(Color.BLUE)
+            .withColor(Color.YELLOW)
+            .withColor(Color.WHITE)
+            .with(FireworkEffect.Type.CREEPER)
+            .build())
+
+        meta.power = 2
+
+        firework.fireworkMeta = meta
+    }
+
+    @EventHandler
+    private fun onJoin(p: PlayerJoinEvent){
+        p.player.addPotionEffect(PotionEffect(PotionEffectType.SPEED, 100, 3))
     }
 
     override fun onDisable() {
