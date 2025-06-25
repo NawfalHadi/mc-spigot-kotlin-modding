d25903f4711202f3c1b47c6ceb41b6a731de69a8
NawfalHadi <nawfalhdi@gmail.com>
2025-06-25 20:21:35 +0800

    items : more detail about item meta

--- src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
+++ src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
@@ -1,17 +1,61 @@
 package com.thatnawfal.mcSpigotKotlinModding
 
 import org.bukkit.Bukkit
+import org.bukkit.Color
 import org.bukkit.Material
 import org.bukkit.entity.Item
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BookMeta
+import org.bukkit.inventory.meta.LeatherArmorMeta
 import org.bukkit.plugin.java.JavaPlugin
 
-class McSpigotKotlinModding : JavaPlugin() {
+class McSpigotKotlinModding : JavaPlugin(), Listener {
 
     override fun onEnable() {
 //        itemStack()
 //        setBlockAt()
 //        checkBlockTypes()
+
+//        itemMetaExplain()
+        givePlayerArmorSpawn()
+    }
+
+    private fun givePlayerArmorSpawn() {
+        Bukkit.getPluginManager().registerEvents(this, this)
+    }
+
+    @EventHandler
+    private fun onJoin(playerJoinEvent: PlayerJoinEvent){
+        val helmet = ItemStack(Material.LEATHER_HELMET)
+        val helmetMeta = helmet.itemMeta as LeatherArmorMeta
+//        the function are on the meta variable
+        helmetMeta.setColor(Color.GREEN)
+//        then set the meta into the helmet
+        helmet.itemMeta = helmetMeta
+
+        val chestPlate = ItemStack(Material.LEATHER_CHESTPLATE)
+        val chestMeta = helmet.itemMeta as LeatherArmorMeta
+        chestMeta.setColor(Color.RED)
+        chestPlate.itemMeta = chestMeta
+
+        playerJoinEvent.player.inventory.addItem(helmet)
+        playerJoinEvent.player.inventory.addItem(chestPlate)
+
+    }
+
+    private fun itemMetaExplain() {
+//        Meta More like function you can change the
+//        function of items to function as another item meta
+
+//        if i left it the bamboo_block they dont have the block meta
+        val itemStack = ItemStack(Material.BAMBOO_BLOCK)
+//        forcing the bamboo having the book meta
+        val theMeta = itemStack.itemMeta as BookMeta
+//        and set it
+        itemStack.itemMeta = theMeta
 
     }
 
