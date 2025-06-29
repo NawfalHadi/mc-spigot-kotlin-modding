c20dc7a4cd89bf40e5921fb06e498911129e54bd
NawfalHadi <nawfalhdi@gmail.com>
2025-06-29 11:08:19 +0800

    env : a little about environments

--- src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
+++ src/main/kotlin/com/thatnawfal/mcSpigotKotlinModding/McSpigotKotlinModding.kt
@@ -1,11 +1,22 @@
 package com.thatnawfal.mcSpigotKotlinModding
 
+import org.bukkit.Bukkit
+import org.bukkit.WorldCreator
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class McSpigotKotlinModding : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+
+        val player: Player? = null
+//        how do i get environment around player
+        player?.world?.setStorm(true)
+
+//        You Can Creating a new wolrd
+        Bukkit.createWorld(WorldCreator("New World"))
+//        This is the default
+        Bukkit.getWorld("world")
     }
 
     override fun onDisable() {
